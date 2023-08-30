b6b7587b7524314642382c94cda93dffe06e690c
Renato França <renatofranca2881@gmail.com>
2023-08-29 21:09:44 -0300

    carrossel 99% pronto

--- app/componentes/carrossel01.js
+++ app/componentes/carrossel01.js
@@ -1,69 +1,64 @@
 export function carrossel01() {
-  const btnProxima = document.querySelector("#proxima");
-  const btnRetornar = document.querySelector("#retornar");
-
   const almoco = document.querySelector('[data-slide="almoco"]');
   const paes = document.querySelector('[data-slide="paes"]');
   const lanches = document.querySelector('[data-slide="lanches"]');
-  const pizzas = document.querySelector('[data-slide="pizzas"]');
+  const pizzas = document.querySelector('[data-slide="pizza"]');
 
-  
-  const proximoTeste = document.querySelectorAll(".proxima");
-  console.log(proximoTeste);
+  const btnProximo = document.querySelectorAll(".proxima");
+  const btnRetornar = document.querySelectorAll(".retornar");
 
-  proximoTeste.forEach((exato) => {
-    exato.addEventListener('click', (qualFoiEm)=>{
-      const local = qualFoiEm.currentTarget.id;
-      const slide = qualFoiEm.currentTarget.parentNode.getAttribute('data-slide');
-
-      if (local === 'almoco') {
-        valorDoEixo += mais;
-        almoco.style.transform = `translateX(${valorDoEixo}px)`;
-      }
-    })
-
-    // queroEsse.addEventListener("click", (esseAqui) => {
-    //   esseAqui.currentTarget.addEventListener('click', (esse)=>{
-    //     console.log('mais um teste: ' + esse);
-    //   });
-    //   const slides = document.querySelector(".vitrine-itens");
-      
-      
-    // });
-  });
-// if (naoPassar === valorDoEixo) {
-        //   esse.style.transform = `translateX(${valorDoEixo}px)`;
-        // } else {
-        //   valorDoEixo += mais;
-        //   esse.style.transform = `translateX(${valorDoEixo}px)`;
-        //   console.log(valorDoEixo);
-        // }
-  const valorDoEixoZerado = 0;
   var valorDoEixo = 0;
-
   let mais = -344;
   let menos = +344;
-
+  const valorDoEixoZerado = 0;
   const naoPassar = -1720;
 
-  // btnPassar
-  // btnProxima.addEventListener("click", () => {
-  //   if (naoPassar === valorDoEixo) {
-  //     slides.style.transform = `translateX(${valorDoEixo}px)`;
-  //   } else {
-  //     valorDoEixo += mais;
-  //     slides.style.transform = `translateX(${valorDoEixo}px)`;
-  //     console.log(valorDoEixo);
-  //   }
-  // });
+  // btn para o proximo item do carrossel
+  btnProximo.forEach((exato) => {
+    exato.addEventListener("click", (qualFoiEm) => {
+      const local = qualFoiEm.currentTarget.id;
 
-  // btnVoltar
-  // btnRetornar.addEventListener("click", () => {
-  //   if (valorDoEixo === valorDoEixoZerado) {
-  //     slides.style.transform = `translateX(${valorDoEixoZerado}px)`;
-  //   } else {
-  //     valorDoEixo += menos;
-  //     slides.style.transform = `translateX(${valorDoEixo}px)`;
-  //   }
-  // });
+      if (local === "almoco") {
+        proximo(almoco);
+      }
+      if (local === "paes") {
+        proximo(paes);
+      }
+      if (local === "lanches") {
+        proximo(lanches);
+      }
+      if (local === "pizzas") {
+        proximo(pizzas);
+      }
+    });
+  });
+
+  // btn para voltar os itens do carrossel
+  btnRetornar.forEach((exato) => {
+    exato.addEventListener("click", (qualFoiEm) => {
+      const local = qualFoiEm.currentTarget.id;
+      if (local === "almoco") {
+        retornar(almoco);
+      }
+      if (local === "paes") {
+        retornar(paes);
+      }
+      if (local === "lanches") {
+        retornar(lanches);
+      }
+      if (local === "pizzas") {
+        retornar(pizzas);
+      }
+    });
+  });
+
+  // funções de proximo e retornar
+  function proximo(next) {
+    valorDoEixo += mais;
+    next.style.transform = `translateX(${valorDoEixo}px)`;
+  }
+  function retornar(retornar) {
+    valorDoEixo += menos;
+    retornar.style.transform = `translateX(${valorDoEixo}px)`;
+  }
 }
